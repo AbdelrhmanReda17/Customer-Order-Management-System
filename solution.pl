9eb7698fd8f19d15e8d5f03f7289733434a76a72
Rana emara <schleidenjakob@gmail.com>
2024-03-23 07:24:45 +0200

    problems 7 & 11 were added

--- solution.pl
+++ solution.pl
@@ -3,12 +3,30 @@
 :-dynamic alternative/2.
 :-dynamic boycott_company/2.
 
+% 3. List all items in a specific customer order given customer id and order id.
+getItemsInOrderById(CustomerId,OrderdId,Items):-
+    order(CustomerId,OrderdId,Items).
+
+
+% 7.Given the company name or an item name, find the justification why you need to boycott this company/item. 
+whyToBoycott(ItemName, Justification) :-
+    item(ItemName, CompanyName, _),
+    boycott_company(CompanyName, Justification).   
+
 % 6. Given the item name or company name, determine whether we need to boycott or not.
 isBoycott(CompanyName) :-
     boycott_company(CompanyName, _).
 isBoycott(ItemName) :-
     item(ItemName,CompanyName,_),
     boycott_company(CompanyName, _).
+
+% 11.calculate the difference in price between the boycott item and its
+%alternative.
+ getTheDifferenceInPriceBetweenItemAndAlternative(ItemName, Alternative, DiffPrice) :-
+    item(ItemName, _, ItemPrice),
+    alternative(ItemName, Alternative),
+    item(Alternative, _, AltPrice),
+    DiffPrice is ItemPrice - AltPrice.
 
 % 12. Insert/Remove (1)item, (2)alternative and (3)new boycott company to/from the knowledge base. Hint: use assert to insert new fact and retract to remove a fact
 % Insert a new item
@@ -66,8 +84,5 @@ removeBoycottItems([H|T], NewList) :-
     removeBoycottItems(T , Remaining)).
 
 
-% 3. List all items in a specific customer order given customer id and order id.
 
-getItemsInOrderById(CustomerId,OrderdId,Items):-
-    order(CustomerId,OrderdId,Items).
 
