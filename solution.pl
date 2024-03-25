9c1a5d7f4e7b7556a9a013a0a207b1987e295370
abdelrahman atef <102483897+AbdoMan19@users.noreply.github.com>
2024-03-25 04:14:43 +0200

    add prolem 1

--- solution.pl
+++ solution.pl
@@ -3,56 +3,24 @@
 :-dynamic alternative/2.
 :-dynamic boycott_company/2.
 
+% 1. List all items in the order given customer id and order id.
+list_orders(CustomerName,Orders) :-
+    customer(CustomerId, CustomerName),
+    orders_by_customer(CustomerId,[],Orders).
+    
+orders_by_customer(CustomerId, OrdersAcc, [H|T]) :-
+    order(CustomerId,OrderId,Items),
+    \+ member(order(CustomerId,OrderId,_), OrdersAcc),
+    H=order(CustomerId, OrderId, Items),
+    orders_by_customer(CustomerId,[H|OrdersAcc],T).
+
+orders_by_customer(_, _, []).
+
+
 % 3. List all items in a specific customer order given customer id and order id.
+
 getItemsInOrderById(CustomerId,OrderdId,Items):-
     order(CustomerId,OrderdId,Items).
-
-
-% 7.Given the company name or an item name, find the justification why you need to boycott this company/item. 
-whyToBoycott(ItemName, Justification) :-
-    item(ItemName, CompanyName, _),
-    boycott_company(CompanyName, Justification).   
-
-% 6. Given the item name or company name, determine whether we need to boycott or not.
-isBoycott(CompanyName) :-
-    boycott_company(CompanyName, _).
-isBoycott(ItemName) :-
-    item(ItemName,CompanyName,_),
-    boycott_company(CompanyName, _).
-
-% 11.calculate the difference in price between the boycott item and its
-%alternative.
- getTheDifferenceInPriceBetweenItemAndAlternative(ItemName, Alternative, DiffPrice) :-
-    item(ItemName, _, ItemPrice),
-    alternative(ItemName, Alternative),
-    item(Alternative, _, AltPrice),
-    DiffPrice is ItemPrice - AltPrice.
-
-% 12. Insert/Remove (1)item, (2)alternative and (3)new boycott company to/from the knowledge base. Hint: use assert to insert new fact and retract to remove a fact
-% Insert a new item
-add_item(ItemName, CompanyName, Price) :-
-    assert(item(ItemName, CompanyName, Price)).
-
-% Remove an item
-remove_item(ItemName) :-
-    retract(item(ItemName, _, _)).
-
-% Insert a new alternative
-insert_alternative(ItemName, AlternativeItem) :-
-    assert(alternative(ItemName, AlternativeItem)).
-
-% Remove an alternative
-remove_alternative(ItemName, AlternativeItem) :-
-    retract(alternative(ItemName, AlternativeItem)).
-
-% Insert a new boycott company
-insert_boycottCompany(CompanyName, Justification) :-
-    assert(boycott_company(CompanyName, Justification)).
-
-% Remove a boycott company
-remove_boycottCompany(CompanyName) :-
-    retract(boycott_company(CompanyName, _)).
-
 
 
 % 4. Get the num of items in a specific customer order given customer Name and order id.
@@ -67,6 +35,21 @@ len([],0).
 len([_|Tail], Count) :- 
     len(Tail, Y),  
     Count is Y + 1.
+
+
+% 6. Given the item name or company name, determine whether we need to boycott or not.
+isBoycott(CompanyName) :-
+    boycott_company(CompanyName, _).
+isBoycott(ItemName) :-
+    item(ItemName,CompanyName,_),
+    boycott_company(CompanyName, _).
+
+
+% 7.Given the company name or an item name, find the justification why you need to boycott this company/item. 
+
+whyToBoycott(ItemName, Justification) :-
+    item(ItemName, CompanyName, _),
+    boycott_company(CompanyName, Justification).   
 
 
 % 8. Given an username and order ID, remove all the boycott items from this order.
@@ -116,6 +99,45 @@ calcPrice([H|T], AccPrice, TotalPrice):-
     item(H, _, Price),
     NewAccPrice is AccPrice + Price,
     calcPrice(T, NewAccPrice, TotalPrice).
+
+
+% 11.calculate the difference in price between the boycott item and its alternative.
+
+ getTheDifferenceInPriceBetweenItemAndAlternative(ItemName, Alternative, DiffPrice) :-
+    item(ItemName, _, ItemPrice),
+    alternative(ItemName, Alternative),
+    item(Alternative, _, AltPrice),
+    DiffPrice is ItemPrice - AltPrice.
+
+
+% 12. Insert/Remove (1)item, (2)alternative and (3)new boycott company to/from the knowledge base. Hint: use assert to insert new fact and retract to remove a fact
+% Insert a new item
+add_item(ItemName, CompanyName, Price) :-
+    assert(item(ItemName, CompanyName, Price)).
+
+% Remove an item
+remove_item(ItemName) :-
+    retract(item(ItemName, _, _)).
+
+% Insert a new alternative
+insert_alternative(ItemName, AlternativeItem) :-
+    assert(alternative(ItemName, AlternativeItem)).
+
+% Remove an alternative
+remove_alternative(ItemName, AlternativeItem) :-
+    retract(alternative(ItemName, AlternativeItem)).
+
+% Insert a new boycott company
+insert_boycottCompany(CompanyName, Justification) :-
+    assert(boycott_company(CompanyName, Justification)).
+
+% Remove a boycott company
+remove_boycottCompany(CompanyName) :-
+    retract(boycott_company(CompanyName, _)).
+
+
+
+
 
 
 
